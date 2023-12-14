25c5ec545dcd68cc623346e387fe3d322617d264
NathanHodo <nhodo@me.com>
2023-12-14 12:52:11 -0800

    boilerplate code ready for development

---
+++ src/main/java/com/revature/revpaynathan/RevpayNathanApplication.java
@@ -0,0 +1,13 @@
+package com.revature.revpaynathan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevpayNathanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevpayNathanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/revpaynathan/RevpayNathanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.revpaynathan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevpayNathanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
